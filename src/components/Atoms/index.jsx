55b9815aba860f03b11f8518abffb6caa685e07d
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-10-16 14:21:39 +0900

    Create index.jsx

---
+++ src/components/Atoms/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import COLOR from "../../../../variables/color";
+import BREAKPOINT from "../../../../variables/breakpoint";
+import TEXT from "../../../../variables/texts";
+import FONTFAMILY from "../../../../variables/font_family";
+
+const Title = () => {
+  return <StyledText>SIMPLE TODO APP</StyledText>;
+};
+export default Title;
+
+const StyledText = styled.div`
+  color: ${COLOR.WHITE};
+  ${TEXT.M}
+  font-family: ${FONTFAMILY.ROBOTO};
+  @media screen(min-width: ${BREAKPOINT.MEDIUM}) {
+    ${TEXT.L}
+  }
+`;
