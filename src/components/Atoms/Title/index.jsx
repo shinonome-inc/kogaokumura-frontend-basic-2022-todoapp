f88846cc05b13968f74fe4674a6b43206d6dd464
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-10-16 14:40:06 +0900

    Update index.jsx

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import COLOR from "../../../../variables/color";
-import BREAKPOINT from "../../../../variables/breakpoint";
-import TEXT from "../../../../variables/texts";
-import FONTFAMILY from "../../../../variables/font_family";
+import COLOR from "../../../variables/color";
+import BREAKPOINT from "../../../variables/breakpoint";
+import TEXT from "../../../variables/texts";
+import FONTFAMILY from "../../../variables/font_family";
 
 const Title = () => {
   return <StyledText>SIMPLE TODO APP</StyledText>;
@@ -14,7 +14,7 @@ const StyledText = styled.div`
   color: ${COLOR.WHITE};
   ${TEXT.M}
   font-family: ${FONTFAMILY.ROBOTO};
-  @media screen(min-width: ${BREAKPOINT.MEDIUM}) {
+  @media screen and (min-width: ${BREAKPOINT.MEDIUM}) {
     ${TEXT.L}
   }
 `;
