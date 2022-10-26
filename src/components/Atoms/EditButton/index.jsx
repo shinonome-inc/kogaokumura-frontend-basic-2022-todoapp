f30b492d5fb0b73513c46cb7707d93c55e85b4a2
kogaokumura <k.okumura.22.5.520@4nonome.com>
2022-10-26 22:54:35 +0900

    a

---
+++ src/components/Atoms/EditButton/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import pencil from "../../../assets/svg/pencil.svg";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+
+const EditButton = ({ checked }) => {
+  return (
+    <StyledButton onClick={checked}>
+      <HoverCircle />
+      <Img src={pencil} />
+    </StyledButton>
+  );
+};
+export default EditButton;
+
+const Img = styled.img`
+  width: 15px;
+  height: 15px;
+  top: 10px;
+`;
+
+const HoverCircle = styled.div`
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${COLOR.LIGHT_GRAY};
+  opacity: 0;
+  transition: 0.2s;
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: none;
+  &:hover > ${HoverCircle} {
+    opacity: 0.2;
+  }
+`;
