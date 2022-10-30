798b1bf4a5303b2df8ff2290a420acf7577d142d
kogaokumura <k.okumura.22.5.520@4nonome.com>
2022-10-30 14:40:16 +0900

    変更

--- src/components/Atoms/EditButton/story.jsx
+++ src/components/Atoms/EditButton/story.jsx
@@ -15,5 +15,5 @@ const Template = (args) => <Component {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  onClick: () => console.log("clicked"),
+  checked: () => console.log("clicked"),
 };
