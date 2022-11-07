2e86e37d9fcadf42392f7721ae2cec3ca6dacc25
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-11-07 16:56:23 +0900

    Create index.jsx

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+
+const Input = ({ defaultValue = "", onEditComplete }) => {
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current.focus();
+    ref.current.value = defaultValue;
+    ref.current.onblur = (e) => onEditComplete(e.target.value);
+    ref.current.onkeypress = (e) => {
+      if (e.key === "Enter") onEditComplete(e.target.value);
+    };
+  }, []);
+
+  return <StyledInput ref={ref} defaultValue={defaultValue} />;
+};
+export default Input;
+
+const StyledInput = styled.input`
+  width: 100%;
+  color: ${COLOR.LIGHT_GRAY};
+  ${TEXT.S}
+  background-color: ${COLOR.BLACK};
+  padding: 0px 4px;
+  border: none;
+  border-radius: 2px;
+  outline: none;
+`;
