3f44037a15cfa14b86e290cef610f68331f8f330
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-11-14 16:06:41 +0900

    Create story.jsx

---
+++ src/components/Molecules/Task/story.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import Component from "./index";
+
+export default {
+  component: Component,
+  title: "Molecules/Task",
+  parameters: {
+    backgrounds: {
+      default: "dark",
+    },
+  },
+};
+
+const Template = (args) => <Component {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  checked: () => console.log("task completed"),
+  taskName: "taskname",
+  onEditComplete: (taskName) => console.log(`taskname changed ${taskName}`),
+};
