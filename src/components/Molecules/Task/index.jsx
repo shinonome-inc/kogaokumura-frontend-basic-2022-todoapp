95f715542830a8187d05d68e2233b616d7b8ee68
koga / Koga Okumura <107789098+kogaokumura@users.noreply.github.com>
2022-12-09 15:25:27 +0900

    Update index.jsx

--- src/components/Molecules/Task/index.jsx
+++ src/components/Molecules/Task/index.jsx
@@ -6,8 +6,8 @@ import Checkbox from "../../Atoms/Checkbox";
 import Input from "../../Atoms/Input";
 import EditButton from "../../Atoms/EditButton";
 
-const Task = ({ onClick, taskName, onEditComplete, defaultIsEditing }) => {
-  const [isEditing, setIsEditing] = useState(defaultIsEditing);
+const Task = ({ onClick, taskName, onEditComplete, defoultIsEditing }) => {
+  const [isEditing, setIsEditing] = useState(true);
 
   const stateEditButton = () => {
     setIsEditing(true);
