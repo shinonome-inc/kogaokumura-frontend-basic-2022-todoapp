925ae7da6d54580364314699a2df04d6ff6b519c
kogaokumura <k.okumura.22.5.520@4nonome.com>
2022-12-11 13:36:03 +0900

    保留

---
+++ src/components/Pages/MainPage/index.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Title from "../../Atoms/Title";
+import TodoCard from "../../Organisms/TodoCard";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+
+const MainPages = () => {};
+<StyledMainPage>
+  <Title></Title>
+  <TodoCard></TodoCard>
+</StyledMainPage>;
