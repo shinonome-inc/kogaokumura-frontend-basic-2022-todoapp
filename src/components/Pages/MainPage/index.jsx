91deaa74da7cfd864dba9c5d00c3c824797c31a5
kogaokumura <k.okumura.22.5.520@4nonome.com>
2022-12-17 14:38:21 +0900

    todocardが編集中になったまま

--- src/components/Pages/MainPage/index.jsx
+++ src/components/Pages/MainPage/index.jsx
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import Title from "../../Atoms/Title";
 import TodoCard from "../../Organisms/TodoCard";
-import styled from "styled-components";
-import COLOR from "../../../variables/color";
 
-const MainPages = () => {};
-<StyledMainPage>
-  <Title></Title>
-  <TodoCard></TodoCard>
-</StyledMainPage>;
+const MainPage = () => {
+  return (
+    <Container>
+      <Title />
+      <TodoCard />
+    </Container>
+  );
+};
+export default MainPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
