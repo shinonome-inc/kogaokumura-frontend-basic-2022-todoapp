54d54561da0b42948639b20c577e174a6f22cfc4
kogaokumura <k.okumura.22.5.520@4nonome.com>
2022-12-24 17:13:20 +0900

    修正

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import COLOR from "../../../variables/color";
 import BREAKPOINT from "../../../variables/breakpoint";
